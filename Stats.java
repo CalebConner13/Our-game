64228bec6f3c6f191f67e2b22a571af28f07279f
CalebConner13 <30534014+CalebConner13@users.noreply.github.com>
2017-08-03 13:42:15 -0400

    Update Stats.java

--- Stats.java
+++ Stats.java
@@ -3,7 +3,14 @@ public class Stats { //Fallout type S.P.E.C.I.A.L system? (or a variant)
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/*int str;
+		 *int per;
+		 *int end;
+		 *int cha;
+		 *int intel;
+		 *int agl;
+		 *int luck;
+		 */
 	}
 
 }
