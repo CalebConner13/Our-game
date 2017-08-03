3817b01bb7c94af7127c3af2112886b278a56c66
CalebConner13 <30534014+CalebConner13@users.noreply.github.com>
2017-08-03 12:53:28 -0400

    Updated Story.java

--- Stats.java
+++ Stats.java
@@ -1,9 +1,9 @@
 
-public class Stats {
-
+public class Stats { //Fallout type S.P.E.C.I.A.L system? (or a variant)
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
