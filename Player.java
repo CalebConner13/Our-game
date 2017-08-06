5d6b4c399751a805addc0185d2534183a60add77
Brent <brentbemiller@gmail.com>
2017-08-05 22:15:44 -0400

    Test

--- Player.java
+++ Player.java
@@ -4,7 +4,7 @@ public class Player {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		System.out.println("What is your name traveller? "); //Asks for player name
+		System.out.println("What is your name? "); //Asks for player name
 		String name = scan.nextLine();
 		scan.close();
 		
